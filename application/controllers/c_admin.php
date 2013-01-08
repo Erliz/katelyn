19bafe6664b01245ca92e782faa62d98f0b6e61d
Stanislav <mrerliz@gmail.com>
2013-01-09 00:13:28 +0400

    Admin passwd fix

--- application/controllers/c_admin.php
+++ application/controllers/c_admin.php
@@ -14,7 +14,7 @@ class C_Admin extends C_Base
         if (!empty($_POST['passwd'])) {
             $phrase = explode('_', trim(strip_tags($_POST['passwd'])));
             $public = md5($phrase[0]) . $phrase[1];
-            $private = $this->settings['passwd'] . date('dG', time());
+            $private = $this->settings['passwd'] . date('dH', time());
             if ($public == $private) {
                 $_SESSION['admin_key'] = $this->settings['admin_key'];
             }
