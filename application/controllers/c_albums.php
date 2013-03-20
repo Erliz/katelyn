438478efe46e1475695aa1d98acf25ac48b768d9
Stanislav Vetlovskiy <mrerliz@gmail.com>
2013-03-21 00:59:11 +0400

    Add pager to albums

--- application/controllers/c_albums.php
+++ application/controllers/c_albums.php
@@ -1,16 +1,25 @@
 <?php
 /**
- *
+ * @author Stanislav Vetlovskiy
+ * @date 09.01.13
  */
 class C_Albums extends C_Base
 {
 
     public function index()
     {
+        $page = 1;
+        if (!empty($_GET['page']) && $_GET['page'] > 0 && preg_match('/[0-9]+/', $_GET['page'])) {
+            $page = strip_tags($_GET['page']);
+        }
         $model = new M_Album();
         Registry::$display->assign(
-            array('request'=>
-                  array('albums'=>$model->getAll()->getCollection())
+            array(
+                'request' => array(
+                    'albums'    => $model->getPage($page)->getCollection(),
+                    'page'      => $page,
+                    'pageCount' => $model->getPagesCount()
+                )
             )
         );
         Registry::$display->disp('page/albums');
@@ -18,21 +27,23 @@ class C_Albums extends C_Base
 
     public function id($get)
     {
-        $id=(int)array_shift($get);
-        if(!preg_match('/[0-9]+/',$id)) $this->index();
+        $id = (int)array_shift($get);
+        if (!preg_match('/[0-9]+/', $id)) {
+            $this->index();
+        }
         $page = (int)(!empty($get[0]) ? $get[0] : 0);
         /** @var $album MO_Album */
-        $album=MO_Album::getById($id);
+        $album = MO_Album::getById($id);
         $album->fillPhotos();
 
         $album->photos->renderPages();
 
         Registry::$display->assign(
-            array('request'=>
-                  array(
-                      'album'=>$album,
-                      'page'=>$page
-                  )
+            array(
+                'request' => array(
+                    'album' => $album,
+                    'page'  => $page
+                )
             )
         );
         Registry::$display->disp('page/album');
--- application/models/m_album.php
+++ application/models/m_album.php
@@ -1,36 +1,59 @@
 <?php
 /**
- * User: Elio
- * Date: 23.12.12
+ * @author Stanislav Vetlovskiy
+ * @date 23.12.12
  *
  */
 class M_Album
 {
+    private $onPage = 6;
 
-    public function createAlbum($title) {
-        $title=trim(strip_tags($title));
-        $album=MO_Album::getByTitle($title);
-        if(!empty($album)){
-            return array('error'=>'Альбом с таким названием уже существует');
+    public function createAlbum($title)
+    {
+        $title = trim(strip_tags($title));
+        $album = MO_Album::getByTitle($title);
+        if (!empty($album)) {
+            return array('error' => 'Альбом с таким названием уже существует');
         }
-        $album=MO_Album::createFromArray(
-            array('title'=>$title,'description'=>'')
+        $album = MO_Album::createFromArray(
+            array('title' => $title, 'description' => '')
         );
-        if($album->saveToBase()){
+        if ($album->saveToBase()) {
             return MO_Album::getByTitle($title);
         } else {
             return false;
         }
     }
 
-    public function getAll() {
-        $collection=new MO_Albums();
-        $stmt=Registry::$db->query("SELECT * FROM `album`");
-        while($row = $stmt->fetch()){
+    public function getAll()
+    {
+        $collection = new MO_Albums();
+        $stmt = Registry::$db->query("SELECT * FROM `album`");
+        while ($row = $stmt->fetch()) {
             $collection->add(MO_Album::fromArray($row));
         }
 
         return $collection;
+    }
+
+    public function getPage($page = 1)
+    {
+        $collection = new MO_Albums();
+        $from = $this->onPage * ($page - 1);
+        $stmt = Registry::$db->query("SELECT * FROM `album` LIMIT $from, {$this->onPage}");
+        while ($row = $stmt->fetch()) {
+            $collection->add(MO_Album::fromArray($row));
+        }
+
+        return $collection;
+    }
+
+    public function getPagesCount()
+    {
+        $stmt = Registry::$db->query("SELECT COUNT(*) as count FROM `album`");
+        $result = $stmt->fetch();
+
+        return ceil($result['count'] / $this->onPage);
     }
 
     /**
@@ -38,8 +61,10 @@ class M_Album
      *
      * @return MO_Album
      */
-    public function getById($id) {
+    public function getById($id)
+    {
         $collection = $this->getAll();
-        return $collection->getItemByVar('id',$id);
+
+        return $collection->getItemByVar('id', $id);
     }
 }
