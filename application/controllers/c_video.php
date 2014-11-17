9be713d5cc1a3ff01e22415a83b34d6c69ab06ab
Stanislav Vetlovskiy <mrerliz@gmail.com>
2014-11-17 22:42:45 +0300

    Add new files

---
+++ application/controllers/c_video.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Stanislav Vetlovskiy
+ * @date 09.01.13
+ */
+class C_Video extends C_Base
+{
+
+    public function index()
+    {
+        Registry::$display->disp('page/video');
+    }
+}
