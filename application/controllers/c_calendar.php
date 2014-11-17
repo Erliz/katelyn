a0126e88025106bad875677c91d237058496187e
Stanislav Vetlovskiy <mrerliz@gmail.com>
2014-11-17 22:40:58 +0300

    Commit old changes

---
+++ application/controllers/c_calendar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Stanislav Vetlovskiy
+ * @date 09.01.13
+ */
+class C_calendar extends C_Base
+{
+
+    public function index()
+    {
+        Registry::$display->disp('page/calendar');
+    }
+}
